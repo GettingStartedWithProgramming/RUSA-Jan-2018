e6b0c7240ea8b04dff6c777aebd6171de9436ef8
YJDave <yashashvidave@gmail.com>
2018-02-03 19:42:07 +0530

    models: Register on admin.py

--- rusa/rusaweb/admin.py
+++ rusa/rusaweb/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import (
+	Program,
+	Participant,
+	Task,
+	TaskCompletion,
+)
 # Register your models here.
+
+admin.site.register(Program)
+admin.site.register(Participant)
+admin.site.register(Task)
+admin.site.register(TaskCompletion)
