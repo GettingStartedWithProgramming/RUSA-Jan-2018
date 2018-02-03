358b487b841d6bdce39dc7f09f15b4dc68ae6995
YJDave <yashashvidave@gmail.com>
2018-02-03 19:52:22 +0530

    models: Update model view in admin.

--- rusa/rusaweb/admin.py
+++ rusa/rusaweb/admin.py
@@ -7,7 +7,27 @@ from .models import (
 )
 # Register your models here.
 
-admin.site.register(Program)
-admin.site.register(Participant)
-admin.site.register(Task)
-admin.site.register(TaskCompletion)
+class ProgramAdmin(admin.ModelAdmin):
+	list_display = ["name", "start_date", "no_of_registrations", "no_of_tasks", "end_date"]
+	class Meta:
+		model = Program
+
+class ParticipantAdmin(admin.ModelAdmin):
+	list_display = ["user_name", "email", "age_group", "program"]
+	class Meta:
+		model = Participant
+
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ["name", "start_date", "no_of_submissions", "program"]
+	class Meta:
+		model = Task
+
+class TaskCompletionAdmin(admin.ModelAdmin):
+	list_display = ["task", "participant", "completion_date"]
+	class Meta:
+		model = TaskCompletion
+
+admin.site.register(Program, ProgramAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskCompletion, TaskCompletionAdmin)
